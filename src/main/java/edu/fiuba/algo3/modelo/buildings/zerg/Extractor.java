8d72bcdb788011bdbe1c6d2f73369c68a35ae9d1
Imasuarezpino <isuarezp@fi.uba.ar>
2022-11-08 15:36:37 -0300

    Test extractor, Caso de uso 4

--- src/main/java/edu/fiuba/algo3/modelo/buildings/zerg/Extractor.java
+++ src/main/java/edu/fiuba/algo3/modelo/buildings/zerg/Extractor.java
@@ -10,11 +10,48 @@ public class Extractor implements Construccion, Turno {
     private int turnosActivo;
     private int vida;
 
+    private int zanganos;
+
+    private int gas;
+
     private static final int TIEMPO_CONSTRUCCION = 6;
+
+    private static final int MIN_ZANGANOS = 1;
+
+    private static final int MAX_ZANGANOS = 3;
+
+    private static final int MULTIPLICADOR_GAS = 10;
 
     public Extractor(){
         turnosActivo = 0;
         vida = 750;
+        zanganos = 0;
+        gas = 0;
+    }
+
+    public void extraer(){
+        if(turnosActivo < TIEMPO_CONSTRUCCION){
+            throw new RuntimeException("Edificio en construccion");
+        }
+        if(zanganos < MIN_ZANGANOS ){
+            throw new RuntimeException("No hay zanganos para trabajar");
+        }
+        gas = zanganos * MULTIPLICADOR_GAS;
+    }
+
+    public void agregarZangano(){
+        if(turnosActivo < TIEMPO_CONSTRUCCION){
+            throw new RuntimeException("Edificio en construccion");
+        }
+        if (zanganos  == MAX_ZANGANOS){
+            throw new RuntimeException("Ya se llego al limite de zanganos");
+        }
+        zanganos++;
+    }
+
+    public int cantidadDeGas(){
+        return gas;
+
     }
 
 
--- src/main/test/edu/fiuba/algo3/entrega_1/Buildings/Zerg/TestsExtractor.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/Buildings/Zerg/TestsExtractor.java
@@ -35,4 +35,238 @@ public class TestsExtractor {
         Assertions.assertThrows(RuntimeException.class, () -> extractor.usar());
     }
 
+    @Test
+    public void unExtractorNoSePuedeExtraerSinZanganosYaConstruido(){
+
+        Extractor unExtractor = new Extractor();
+        boolean afirmacion = false;
+
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+
+        try{
+            unExtractor.extraer();
+        }catch (RuntimeException e){
+            afirmacion = true;
+        }
+
+        assert(afirmacion);
+    }
+
+    @Test
+    public void unExtractorPuedeSumarZanganosTeniendoCero(){
+
+        Extractor unExtractor = new Extractor();
+        boolean afirmacion = true;
+
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+
+
+
+        try{
+            unExtractor.agregarZangano();
+        }catch (RuntimeException e){
+            afirmacion = false;
+        }
+
+        assert(afirmacion);
+    }
+
+    @Test
+    public void unExtractorPuedeSumarZanganosTeniendoUno(){
+
+        Extractor unExtractor = new Extractor();
+        boolean afirmacion = true;
+
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+
+        unExtractor.agregarZangano();
+
+
+
+        try{
+            unExtractor.agregarZangano();
+        }catch (RuntimeException e){
+            afirmacion = false;
+        }
+
+        assert(afirmacion);
+    }
+
+    @Test
+    public void unExtractorPuedeSumarZanganosTeniendoDos(){
+
+        Extractor unExtractor = new Extractor();
+        boolean afirmacion = true;
+
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+
+        unExtractor.agregarZangano();
+        unExtractor.agregarZangano();
+
+
+
+        try{
+            unExtractor.agregarZangano();
+        }catch (RuntimeException e){
+            afirmacion = false;
+        }
+
+        assert(afirmacion);
+    }
+
+    @Test
+    public void unExtractorNoPuedeSumarZanganosTeniendoTres(){
+
+        Extractor unExtractor = new Extractor();
+        boolean afirmacion = false;
+
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+
+        unExtractor.agregarZangano();
+        unExtractor.agregarZangano();
+        unExtractor.agregarZangano();
+
+
+
+        try{
+            unExtractor.agregarZangano();
+        }catch (RuntimeException e){
+            afirmacion = true;
+        }
+
+        assert(afirmacion);
+    }
+
+    @Test
+    public void unExtractorNoPuedeExtraerGasTeniendoCeroZanganos(){
+
+        Extractor unExtractor = new Extractor();
+        boolean afirmacion = false;
+
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+
+
+
+
+
+        try{
+            unExtractor.extraer();
+        }catch (RuntimeException e){
+            afirmacion = true;
+        }
+
+        assert(afirmacion);
+    }
+
+    @Test
+    public void unExtractorPuedeExtraerGasTeniendoUnZangano(){
+
+        Extractor unExtractor = new Extractor();
+        boolean afirmacion = true;
+
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.agregarZangano();
+
+        try{
+            unExtractor.extraer();
+        }catch (RuntimeException e){
+            afirmacion = false;
+        }
+
+        if(unExtractor.cantidadDeGas() != 10){
+            afirmacion = false;
+        }
+
+        assert(afirmacion);
+    }
+
+    @Test
+    public void unExtractorPuedeExtraerGasTeniendoDosZangano(){
+
+        Extractor unExtractor = new Extractor();
+        boolean afirmacion = true;
+
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.agregarZangano();
+        unExtractor.agregarZangano();
+
+        try{
+            unExtractor.extraer();
+        }catch (RuntimeException e){
+            afirmacion = false;
+        }
+
+        if(unExtractor.cantidadDeGas() != 20){
+            afirmacion = false;
+        }
+
+        assert(afirmacion);
+    }
+    @Test
+    public void unExtractorPuedeExtraerGasTeniendoTresZangano(){
+
+        Extractor unExtractor = new Extractor();
+        boolean afirmacion = true;
+
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.pasarTurno();
+        unExtractor.agregarZangano();
+        unExtractor.agregarZangano();
+        unExtractor.agregarZangano();
+        unExtractor.extraer();
+
+
+
+        if(unExtractor.cantidadDeGas() != 30){
+            afirmacion = false;
+        }
+
+        assert(afirmacion);
+    }
+
+
 }
