55101fb7d00ad022bf14aefe6a313ddb500d54e1
Imasuarezpino <isuarezp@fi.uba.ar>
2022-11-07 23:13:59 -0300

    ReservaDeReproduccion
    implemente el inicializador, no estoy seguro como se implementa la evolucion larvas ya que <<TBD>>

--- src/main/java/edu/fiuba/algo3/modelo/buildings/zerg/ReservaDeReproduccion.java
+++ src/main/java/edu/fiuba/algo3/modelo/buildings/zerg/ReservaDeReproduccion.java
@@ -4,6 +4,24 @@ import edu.fiuba.algo3.modelo.Construccion;
 
 public class ReservaDeReproduccion implements Construccion {
 
+    private int puntosDeVida;
+
+    private int turnosActivo;
+
+    private static final int TIEMPO_CONSTRUCCION = 12;
+
+    public ReservaDeReproduccion(){
+        puntosDeVida = 1000;
+        turnosActivo = 0;
+    }
+
+    public void evolucionarLarvas(int larvasEvolucionar)throws RuntimeException{
+        if(turnosActivo < TIEMPO_CONSTRUCCION)
+            throw new RuntimeException("Edificio en construccion");
+    }
+    public void pasarTurno() {
+        turnosActivo++;
+    }
     public void usar() {
 
     }
