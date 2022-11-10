5a66b3ad4556b667c2c01f7697989bcfd134fb02
Imasuarezpino <isuarezp@fi.uba.ar>
2022-11-10 11:31:26 -0300

    Planteo de idea de expancion de moho

--- src/main/java/edu/fiuba/algo3/modelo/Tablero.java
+++ src/main/java/edu/fiuba/algo3/modelo/Tablero.java
@@ -1,6 +1,7 @@
 package edu.fiuba.algo3.modelo;
 
 import edu.fiuba.algo3.modelo.tiles.FloorType;
+import edu.fiuba.algo3.modelo.tiles.Moho;
 import edu.fiuba.algo3.modelo.tiles.Tile;
 
 public class Tablero {
@@ -29,6 +30,26 @@ public class Tablero {
     public void construirEn(int posX, int posY, Construccion estructura){
         //a lo mejor debe recibir la excepcion que le manda construir en caso de error.
         (tabla [posX][posY]).construir(estructura);
+    }
+
+    public void expandirMoho(){
+        for(int i=0; i < maxX; i++){
+            for (int j =0; j < maxY; j++)
+                if((tabla[i][j]).getConstruccion() == Moho ){
+                    for(int k=i; 5 >= (k-i); k++){
+                        tabla[k][j] = new Moho();
+                    }
+                    for(int k=i; (-5) <= (k-i); k--){
+                        tabla[k][j] = new Moho();
+                    }
+                    for(int k=j; 5 >= (k-i); k++){
+                        tabla[i][k] = new Moho();
+                    }
+                    for(int k=j; (-5) <= (k-i); k--){
+                        tabla[i][k] = new Moho();   //Plantie una idea aasi, quizas esta mal
+                    }
+                }
+        }
     }
 
 }
