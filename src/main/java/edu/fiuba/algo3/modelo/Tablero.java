9ae7e3ed8b3fd3a71078f00cb3ad5b750d57a54a
Ezequiel Tosto <tostoezequiel1@gmail.com>
2022-11-10 23:46:07 -0300

    a

--- src/main/java/edu/fiuba/algo3/modelo/Tablero.java
+++ src/main/java/edu/fiuba/algo3/modelo/Tablero.java
@@ -3,6 +3,9 @@ package edu.fiuba.algo3.modelo;
 import edu.fiuba.algo3.modelo.tiles.FloorType;
 import edu.fiuba.algo3.modelo.tiles.Tile;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tablero {
 
     private Tile[][] tabla;
@@ -31,4 +34,31 @@ public class Tablero {
         (tabla [posX][posY]).construir(estructura);
     }
 
+    public List<Tile> getNeighbours(int col, int row) {
+        List<Tile> neighbors = new ArrayList<>();
+        //find all serouding cell by adding +/- 1 to col and row
+        for (int colNum = col - 1 ; colNum <= (col + 1) ; colNum +=1  ) {
+            for (int rowNum = row - 1 ; rowNum <= (row + 1) ; rowNum +=1  ) {
+                //if not the center cell
+                if(! ((colNum == col) && (rowNum == row))) {
+                    //make sure it is within  grid
+                    if(withinGrid (colNum, rowNum)) {
+                        System.out.println("Neighbor of "+ col+ " "+ row + " - " + colNum +" " + rowNum );
+                        neighbors.add(tabla[col][row]);
+                    }
+                }
+            }
+        }
+        return neighbors;
+    }
+
+    private boolean withinGrid(int colNum, int rowNum) {
+        if((colNum < 0) || (rowNum <0) ) {
+            return false;
+        }
+        if((colNum >= maxX) || (rowNum >= maxY)) {
+            return false;
+        }
+        return true;
+    }
 }
