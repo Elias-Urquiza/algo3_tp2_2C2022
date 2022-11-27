86616409760d210161dd1d6292f62974d3d65772
Mateo Ezequiel Ibañez <116282389+MateoIbaniez@users.noreply.github.com>
2022-11-27 18:35:51 -0300

    hay espacios aereos y solo las unidades aereas pueden moverse en ellos.

---
+++ src/main/java/edu/fiuba/algo3/modelo/unidades/zerg/Devorador.java
@@ -0,0 +1,26 @@
+package edu.fiuba.algo3.modelo.unidades.zerg;
+
+import edu.fiuba.algo3.modelo.Economia;
+import edu.fiuba.algo3.modelo.Posicion;
+import edu.fiuba.algo3.modelo.TipoDeUnidades;
+import edu.fiuba.algo3.modelo.unidades.Aire;
+import edu.fiuba.algo3.modelo.unidades.Tierra;
+import edu.fiuba.algo3.modelo.unidades.UnidadZerg;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Devorador extends UnidadZerg {
+
+    public Devorador(Economia economia, Posicion pos) {
+
+        super(200, 150, 50, economia, pos, 4, 5, new Aire(0), new Aire(0));
+
+        ataques.add(new Aire(15));
+    }
+
+    @Override
+    public void agregate(HashMap<TipoDeUnidades, LinkedList> listas) {
+        listas.get(TipoDeUnidades.ZERG).add(this);
+    }
+}
---
+++ src/main/java/edu/fiuba/algo3/modelo/unidades/zerg/Guardian.java
@@ -0,0 +1,26 @@
+package edu.fiuba.algo3.modelo.unidades.zerg;
+
+import edu.fiuba.algo3.modelo.Economia;
+import edu.fiuba.algo3.modelo.Posicion;
+import edu.fiuba.algo3.modelo.TipoDeUnidades;
+import edu.fiuba.algo3.modelo.unidades.Aire;
+import edu.fiuba.algo3.modelo.unidades.Tierra;
+import edu.fiuba.algo3.modelo.unidades.UnidadZerg;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Guardian extends UnidadZerg {
+
+    public Guardian(Economia economia, Posicion pos) {
+
+        super(100, 50, 100, economia, pos, 4, 10, new Aire(0),new Aire(0));
+
+        ataques.add(new Tierra(25));
+    }
+
+    @Override
+    public void agregate(HashMap<TipoDeUnidades, LinkedList> listas) {
+        listas.get(TipoDeUnidades.ZERG).add(this);
+    }
+}
