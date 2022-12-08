6436190a1d4bbdfb693e07d3b79dc55198365115
Mateo Ezequiel Ibañez <116282389+MateoIbaniez@users.noreply.github.com>
2022-12-08 16:10:19 -0300

    Changes by tosto

--- src/main/java/edu/fiuba/algo3/Vista/App.java
+++ src/main/java/edu/fiuba/algo3/Vista/App.java
@@ -1,6 +1,7 @@
 package edu.fiuba.algo3.Vista;
 
 import edu.fiuba.algo3.SystemInfo;
+import edu.fiuba.algo3.controllers.GameController;
 import edu.fiuba.algo3.modelo.Economia;
 import edu.fiuba.algo3.modelo.buildings.zerg.Criadero;
 import edu.fiuba.algo3.modelo.tiles.Manager;
@@ -20,10 +21,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
         // usamos una Vbox y una Hbox
+
+        /* Mover a vista del juego
         CajaDeAbajo cajaAbajo = new CajaDeAbajo();
         CajaDeLaDerecha cajaDeLaDerecha = new CajaDeLaDerecha();
         StackPane stackPane = new StackPane(cajaAbajo, cajaDeLaDerecha);
@@ -36,15 +36,13 @@ public class App extends Application {
 
 
         stage.show();
+        */
+        GameController controller = new GameController(stage);
+        controller.iniciarPantallaSeleccion();
     }
 
     public static void main(String[] args) {
-
-        /*Economia econ = new Economia();
-        econ.ingresarMineral(1000000000);
-        econ.ingresarGasVespeno(100000000);
-        Manager m = new Manager(30, 30);
-        */launch();
+        launch();
     }
 
 
